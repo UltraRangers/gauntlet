25cc60cf1a6c9ccc34dfc1a66795b4bd18c9f64d
jmaicaaan <jmsantosss29@yahoo.com>
2018-03-17 21:03:12 +0800

    Fixed imports and nestFactory.creation

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,20 +1,22 @@
-const express = require('express');
 import { NestFactory } from '@nestjs/core';
 import { Application, Request, Response } from 'express';
 import { resolve } from 'path';
+
+import express = require('express');
+
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const port = 3000 || process.env.NODE_PORT;
   const expressApp = express();
-  const app = await NestFactory.create(AppModule, expressApp);
-  setUpClient(expressApp);
+  const app = await NestFactory.create(AppModule, expressApp, {});
+  setupClient(expressApp);
   await app.init();
   await app.listen(port);
   console.log(`server listening at http://localhost:${port}`);
 }
 
-function setUpClient(expressApp: Application) {
+function setupClient(expressApp: Application) {
   expressApp.use(express.static(resolve(__dirname, '../../dist/client')));
   expressApp.get('*', (request: Request, response: Response) => {
     if (request.path.match(/\.(html|css|png|jpg|ttf|js|ico)$/)) {
