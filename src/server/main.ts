875d09f74774f072e5797f11205a7348f8def36b
cedrick mandocdoc <cedrickmandocdoc@gmail.com>
2018-03-19 20:44:26 +0800

    fix typescript configuration in server

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,5 +1,6 @@
 import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { resolve } from 'path';
 
 import express = require('express');
 
@@ -22,3 +23,23 @@ export async function createNestApplication() {
 export function applyNestApplicationSettings(nestApplication: INestApplication) {
   nestApplication.setGlobalPrefix('api');
 }
+
+function setupClient(expressApp: express.Application) {
+  expressApp.use(express.static(resolve(__dirname, '../../dist/client')));
+  expressApp.get('*', (request: express.Request, response: express.Response) => {
+    if (request.path.match(/\.(html|css|png|jpg|ttf|js|ico)$/)) {
+      return response.status(404)
+        .send('Not found');
+    }
+    response.sendFile(resolve(__dirname, '../../dist/client/index.html'));
+  });
+}
+
+async function start() {
+  const port = process.env.PORT || 3000;
+  const app = await createNestApplication();
+  setupClient(app.expressApp);
+  await app.nestApp.listen(port, () => { console.log(`server listening at http://localhost:${port}`); });
+}
+
+start();
