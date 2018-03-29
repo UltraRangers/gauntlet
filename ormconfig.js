f14d2505667f1c883a8808f875fac6adb733472b
jmaicaaan <jmsantosss29@yahoo.com>
2018-03-29 12:12:29 +0800

    Added heroku setup

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,14 @@
 const env = process.env.NODE_ENV || 'development';
-const config = require(process.cwd() + `/config/server/ormconfig.${env}.json`);
-
-config.entities = [ __dirname + '/dist/server/entities/*.entity.js' ];
-config.migrations = [ __dirname + '/dist/server/utils/migrations/*.js' ];
-config.cli = { migrationsDir: '/src/server/utils/migrations/' };
+let config;
+try {
+  config = require(process.cwd() + `/config/server/ormconfig.${env}.json`);
+} catch (error) {
+  // get the development config as the fallback
+  config = require(process.cwd() + `/config/server/ormconfig.development.json`);
+} finally {
+  config.entities = [ __dirname + '/dist/server/entities/*.entity.js' ];
+  config.migrations = [ __dirname + '/dist/server/utils/migrations/*.js' ];
+  config.cli = { migrationsDir: '/src/server/utils/migrations/' };  
+}
 
 module.exports = config;
