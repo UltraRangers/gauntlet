895ce69927ab004e7cebedac68218038cd8e28c4
jmaicaaan <jmsantosss29@yahoo.com>
2018-03-31 14:05:03 +0800

    Fixed tests with database not disconnecting

--- src/server/users/getUsers.controller.test.ts
+++ src/server/users/getUsers.controller.test.ts
@@ -6,8 +6,12 @@ import { Test } from '@nestjs/testing';
 import { setupNestApplication } from '../setup';
 import { UserModule } from './user.module';
 
+import { DatabaseService } from '../common/database.service';
+import { DatabaseModule } from '../database.module';
+
 describe('GetUsersController', () => {
   const server = express();
+  let databaseService: DatabaseService;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
@@ -15,8 +19,14 @@ describe('GetUsersController', () => {
     }).compile();
 
     const app = await module.createNestApplication(server);
+    databaseService = app.select(DatabaseModule).get(DatabaseService);
+
     setupNestApplication(app);
     await app.init();
+  });
+
+  afterAll(async () => {
+    await databaseService.disconnect();
   });
 
   it('should return 200 /api/users', () => {
