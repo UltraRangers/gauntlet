e688a8e2bf51114a10ec578e70c8df5e48b5cd63
jmaicaaan <jmsantosss29@yahoo.com>
2018-03-31 15:39:38 +0800

    Fixed wrong heroku password env

--- config/server/ormconfig.heroku.js
+++ config/server/ormconfig.heroku.js
@@ -3,6 +3,6 @@ module.exports = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   username: process.env.DB_USER,
-  password: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 }
