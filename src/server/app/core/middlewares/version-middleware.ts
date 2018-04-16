a6043f44398555216d875f01bf7c1fa6a8baa420
cmandocdoc <cmandocdoc@isbx.com>
2018-04-16 08:02:34 +0800

    use process.cwd() in requiring package

--- src/server/app/core/middlewares/version-middleware.ts
+++ src/server/app/core/middlewares/version-middleware.ts
@@ -1,8 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
+import { join } from 'path';
 
-const version = require('../../../../../package.json').version;
+const version = require(join(process.cwd(), 'package.json')).version;
 
 export const versionMiddleware = (request: Request, response: Response, next: NextFunction) => {
+  console.log(version);
   response.set('X-Version', version);
   next();
 };
